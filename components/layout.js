7a7be57264e3b5c422c07c65ab91a0e2f6be6a39
Jack Keller <149119490+hokagejack@users.noreply.github.com>
2024-10-02 21:14:01 -0400

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -1,9 +1,12 @@
 import Nav from './Nav';
 
+// define layout component
 export default function Layout({ children }) {
   return (
     <div className="mx-6 md:max-w-2x1 px-10 md:mx-auto font-nunito">
+      //render navigation component
       <Nav />
+      // render main content
       <main>{children}</main>
     </div>
   )

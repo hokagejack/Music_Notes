7936d030f7e2db74d4f90a64e10889f13a3cb8c6
Jack Keller <149119490+hokagejack@users.noreply.github.com>
2024-10-02 21:07:57 -0400

    Update Nav.js

--- components/Nav.js
+++ components/Nav.js
@@ -8,15 +8,15 @@ export default function Nav() {
 
   return (
     <nav className="flex justify-between items-center py-10">
-    /* link to the homepage */
+    {/* link to homepage */}
       <Link href="/"> 
         <button className="px-10 text-lg font-medium active:text-gray-500 hover:text-gray-400"> Music Notes: Home </button>
       </Link>
 
-      {/* Conditional rendering based on user authentication status */}
+      //join rendering based on auth status
       <ul className="flex items-center gap-10">
         {!user && (
-          // If the user is not logged in, show a "Join Now" button linking to the login page
+          // if user not logged in show a "Join Now" button linking to the login page
           <Link href={"/auth/login"} legacyBehavior> 
             <a className="py-2 px-4 text-sm bg-green-600 text-white rounded-lg hover:bg-green-400 font-medium active:bg-green-200">
               Join Now
@@ -24,19 +24,19 @@ export default function Nav() {
           </Link>
         )}
         {user && (
-          // If the user is logged in, show buttons to post and a profile image linking to the dashboard
+          // if user is logged in, show buttons to post and a profile image linking to dashboard
           <div className="flex items-center gap-6 px-10">
-            {/* Link to the post creation page */}
+            //link to post creation page
             <Link href="/post" >
               <button className="font-medium bg-green-600 active:bg-green-200 hover:bg-green-400 text-white py-2 px-2 rounded-mg textx-sm">
                 Post Here
               </button>
             </Link>
 
-            {/* Profile picture that links to the user's dashboard */}
+            //profile picture that links to the user's dashboard
             <Link href="/dashboard" >
               <img 
-                src={user.photoURL}  // Display the user's profile picture from Firebase auth
+                src={user.photoURL}  // display user's profile picture from Firebase auth
                 className="w-12 rounded-full cursor-pointer hover:opacity-90 active:opacity-60"
               />
             </Link>
